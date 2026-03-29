8240819f341a1809aeb344f606ea905a252e92c0
Amin Nasiri <aminnassiri63@gmail.com>
2026-03-29 14:00:53 -0400

    Update LeetCode 15.py

--- LeetCode 15.py
+++ LeetCode 15.py
@@ -1,9 +1,35 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+# 15. 3Sum
 
+
+# HashMap
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        hm = {}
+        res = set()
+        n = len(nums)
+
+        for k, v in enumerate(nums):
+            hm[v] = k
+
+        for i in range(n):
+            for j in range(i+1, n):
+                rem = -nums[i] - nums[j]
+
+                if rem in hm and hm[rem] != i and hm[rem] != j:
+                    res.add(tuple(sorted([nums[i], nums[j], rem])))
+
+        res = list(map(list, res))
+        # res = [list(tup) for tup in res]
+
+        return res
+
+
+# Two Pointers
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         n = len(nums)
-        ans = []
+        res = []
 
         for i in range(n):
             if nums[i] > 0:
@@ -11,20 +37,24 @@ class Solution:
             elif i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            l, h = i+1, n-1
-            while l < h:
-                s = nums[i] + nums[l] + nums[h]
+            lp, rp = i+1, n-1
+            while lp < rp:
+                summ = nums[i] + nums[lp] + nums[rp]
 
-                if s == 0:
-                    ans.append([nums[i], nums[l], nums[h]])
-                    l, h = l+1, h-1
+                if summ == 0:
+                    res.append([nums[i], nums[lp], nums[rp]])
 
-                    while l < h and nums[l] == nums[l-1]:
-                        l += 1
-                    while l < h and nums[h] == nums[h+1]:
-                        h -= 1
-                elif s < 0:
-                    l += 1
+                    lp, rp = lp+1, rp-1
+                    while lp < rp and nums[lp] == nums[lp-1]:
+                        lp += 1
+                    while lp < rp and nums[rp] == nums[rp+1]:
+                        rp -= 1
+
+                elif summ < 0:
+                    lp += 1
+
                 else:
-                    h -= 1
-        return ans
+                    rp -= 1
+
+        return res
+
